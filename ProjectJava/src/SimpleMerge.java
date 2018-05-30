a9712ee068a32f85e797b63dd0f284a48b748934
Uneishi <Uneishi@users.noreply.github.com>
2018-05-30 20:26:36 +0900

    1rst Java project

---
+++ ProjectJava/src/SimpleMerge.java
@@ -0,0 +1,22 @@
+
+//import ???
+
+public class SimpleMerge {
+
+	public void main(){
+		
+	}
+	
+	public boolean Save(String filename1, String filename2){
+		
+	}
+	
+	public boolean Load(String filename1, String filename2){
+		
+	}
+
+	public boolean Edit(String filename1){
+		
+	}
+
+}
