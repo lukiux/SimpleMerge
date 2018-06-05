2daa36b0f138fe4ee0738597c3ac3e78cd14b473
lukiux <lukaukas@gmail.com>
2018-06-06 02:02:14 +0900

    Update source 6/6

---
+++ ProjectJava/Project17/src/project17/Compare.java
@@ -0,0 +1,13 @@
+package project17;
+
+import javax.swing.JTextArea;
+
+public class Compare {
+	private JTextArea leftArea;
+	private JTextArea rightArea;
+	
+	public Compare(JTextArea lArea, JTextArea rArea) {
+		this.leftArea = lArea;
+		this.rightArea = rArea;
+	}
+}
---
+++ ProjectJava/Project17/src/project17/SimpleMergeMVC.java
@@ -0,0 +1,13 @@
+package project17;
+
+import java.util.*;
+import java.lang.*;
+
+
+public class SimpleMergeMVC {
+	
+	public static void main(String[] args) {
+		
+		SimpleMergeController controller = new SimpleMergeController();		
+	}
+}
